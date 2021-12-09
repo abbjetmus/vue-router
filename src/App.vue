b3fa19e4c1b08aa5034210096ca93ab154a203b9
abbjetmus <jeton.mustini@gmail.com>
2021-12-09 11:43:38 +0100

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <ul>
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/contact">Contact</RouterLink></li>
+        <li><RouterLink to="/about">About</RouterLink></li>
+      </ul>
+    </div>
+
+    <RouterView></RouterView>
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+
+  },
+  data () {
+    return {
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
+// Vue appen
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vue-router 
+import {createRouter, createWebHashHistory} from "vue-router";
+
+// Sidor som vi vill kunna navigera till
+import HomePage from './pages/HomePage.vue'
+import AboutUsPage from './pages/AboutUsPage.vue'
+import ContactUsPage from './pages/ContactUsPage.vue'
+
+// Definerar rutt för sidorna
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/about', component: AboutUsPage },
+  { path: '/contact', component: ContactUsPage }
+]
+
+// Skapar routern
+const router = createRouter ({
+    history: createWebHashHistory(),
+    routes,
+})
+
+// Meddelar Vue att använda våran router
+createApp(App).use(router).mount('#app')
